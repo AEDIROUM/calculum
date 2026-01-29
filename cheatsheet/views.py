7baafbc639ccd52e8bd91031409861d444a380d4
Vic-Nas <nasci.victorio@gmail.com>
2026-01-29 02:02:54 -0500

    Updated cheatsheet view

--- cheatsheet/views.py
+++ cheatsheet/views.py
@@ -6,11 +6,11 @@ from cheatsheet.models import Algorithm, AlgorithmCategory
 def cheatsheet(request: HttpRequest) -> HttpResponse:
     """Display all algorithms organized by category"""
     
-    # Get categories with their algorithms
-    categories = AlgorithmCategory.objects.prefetch_related('algorithms').all()
+    # Get categories with their algorithms, sorted alphabetically
+    categories = AlgorithmCategory.objects.prefetch_related('algorithms').order_by('name')
     
     # Also get algorithms without a category
-    uncategorized = Algorithm.objects.filter(category__isnull=True)
+    uncategorized = Algorithm.objects.filter(category__isnull=True).order_by('title')
     
     context = {
         'categories': categories,
