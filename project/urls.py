f7d2812ec1e5735a2d0b262821f7ef394d1fb08c
Vic-Nas <nasci.victorio@gmail.com>
2026-01-29 00:52:58 -0500

    Served media file

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.static import serve
 from django.conf import settings
 
@@ -30,3 +30,11 @@ urlpatterns = [
     path('events', include('events.urls')),
     path('', include('info.urls')),
 ]
+
+# Serve media files in production
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
