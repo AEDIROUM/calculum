920a3e5b616bb45635558aab4b60d6ec54acf0ca
Vic-Nas <nasci.victorio@gmail.com>
2026-01-29 01:24:27 -0500

    Fixed missing models

--- board/admin.py
+++ board/admin.py
@@ -1,22 +1,18 @@
-
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from board.models import Meet, Problem, Session
 
-class CustomAdminSite(admin.AdminSite):
-	site_header = "Edit website"
-	site_title = "Calculum Edit"
-	index_title = "Applications"
-	site_url = None
-	
-	def index(self, request, extra_context=None):
-		extra_context = extra_context or {}
-		return super().index(request, extra_context)
 
-admin.site = CustomAdminSite()
+# Customize the existing admin site instead of replacing it
+admin.site.site_header = "Edit website"
+admin.site.site_title = "Calculum Edit"
+admin.site.index_title = "Applications"
+admin.site.site_url = None
+
 
 admin.site.register(Meet)
+
 
 class UserAdmin(BaseUserAdmin):
 	def has_add_permission(self, request):
@@ -31,7 +27,11 @@ class UserAdmin(BaseUserAdmin):
 	def has_view_permission(self, request, obj=None):
 		return request.user.is_superuser
 
+
+# Unregister the default User admin and register our custom one
+admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
 
 class ProblemAdmin(admin.ModelAdmin):
 	search_fields = ['platform', 'link', 'solution_link']
@@ -39,6 +39,7 @@ class ProblemAdmin(admin.ModelAdmin):
 	def get_ordering(self, request):
 		# Sort by platform and extracted title (as in __str__)
 		return ['platform', 'link']
+
 
 admin.site.register(Problem, ProblemAdmin)
 admin.site.register(Session)
