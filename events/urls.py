96b3f67a9f6d20f3a96ad7c9066f2ae4fc3b307a
Vic-Nas <nasci.victorio@gmail.com>
2026-02-11 12:36:37 -0500

    Correction chemin events

--- events/urls.py
+++ events/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.events, name='events'),
+    path('/', views.events, name='events'),
     # Proxy route - catches /events/<slug> and optionally /events/<slug>/path
     # The trailing slash and anything after is optional
     re_path(r'^(?P<slug>[\w-]+)/?(?P<path>.*)$', views.event_proxy, name='event_proxy'),
